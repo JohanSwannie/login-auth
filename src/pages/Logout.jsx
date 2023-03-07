5eb9d291b04579d13376fca5b64305679c07a389
jswan <jswannieman@gmail.com>
2023-03-08 10:12:42 +1100

    Update Logout jsx component

---
+++ src/pages/Logout.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+
+export default function Logout() {
+  const navigate = useNavigate();
+  const [cookies, setCookie, removeCookie] = useCookies([]);
+  useEffect(() => {
+    const verifyUser = async () => {
+      if (!cookies.jwt) {
+        navigate("/login");
+      } else {
+        const { data } = await axios.post(
+          "http://localhost:4000",
+          {},
+          {
+            withCredentials: true,
+          }
+        );
+        alert(data.status);
+        if (!data.status) {
+          removeCookie("jwt");
+          navigate("/login");
+        } else
+          toast(`Hi ${data.user}`, {
+            theme: "dark",
+          });
+      }
+    };
+    verifyUser();
+  }, [cookies, navigate, removeCookie]);
+
+  const logOut = () => {
+    removeCookie("jwt");
+    navigate("/register");
+  };
+  return (
+    <>
+      <div className="private">
+        <h1>Logout Page</h1>
+        <button onClick={logOut}>Log out</button>
+      </div>
+      <ToastContainer />
+    </>
+  );
+}
